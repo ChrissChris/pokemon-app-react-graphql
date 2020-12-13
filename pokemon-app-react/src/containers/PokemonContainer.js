224329281f0065c4df4faf481455fcbca479f5a5
Chriss Chris <zenskech@outlook.com>
2020-12-13 23:32:01 +0500

    Update PokemonContainer.js

--- pokemon-app-react/src/containers/PokemonContainer.js
+++ pokemon-app-react/src/containers/PokemonContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
+import { Pokemon } from "../components/Pokemon";
 
 export function PokemonsContainer() {
   return <p>I am a pokemon container</p>;
