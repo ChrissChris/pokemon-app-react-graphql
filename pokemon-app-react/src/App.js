1aa534b2e950499ece7bebfc70b804d05b415444
Chriss Chris <zenskech@outlook.com>
2020-12-13 22:44:19 +0500

    Update App.js

--- pokemon-app-react/src/App.js
+++ pokemon-app-react/src/App.js
@@ -6,7 +6,13 @@ function App() {
   const client = new ApolloClient({
     url: "https://graphql-pokemon.now.sh",
   });
-  return <ApolloProvider client={client}></ApolloProvider>;
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
+  );
 }
 
 export default App;
